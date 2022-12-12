acda1cfd3b47b85d82d23ccb4575e9fe9b8ec583
wawatai <wataiPublic@gmail.com>
2022-12-12 18:14:01 +0800

    111-12-12

--- js/js.js
+++ js/js.js
@@ -15,6 +15,41 @@ $(function(){
             prevEl: '.swiper-button-prev',
           },
     });
+    var betswiper = new Swiper('.betswiper', {
+        autoHeight: true,
+        loop: true,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+        },
+        spaceBetween: 0,
+        slidesPerView: 3,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.swiper-button-next02',
+            prevEl: '.swiper-button-prev02',
+          },
+    });
+})
+//appAD
+$(function(){
+    $(".appAD .getIN").hover(function(){
+        $(".coinAll")
+        .addClass("active");
+        $(".light01")
+        .addClass("active");
+        $(".light02")
+        .addClass("active");
+    }, function(){
+        $(".coinAll")
+        .removeClass("active");
+        $(".light01")
+        .removeClass("active");
+        $(".light02")
+        .removeClass("active");
+    })
+
 })
 //aside
 $(function(){
